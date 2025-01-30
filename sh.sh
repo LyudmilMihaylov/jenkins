a7000fa1b9b1783d77256653cfb47229943b2cc9
Lyudmil Borislavov Mihaylov <131765807+LyudmilMihaylov@users.noreply.github.com>
2025-01-30 14:02:49 +0000

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# First stage: List files in the current directory (similar to 'ls' command)
+echo "Listing files in the current directory:"
+ls
+
+# Second stage: Print the current working directory (similar to 'pwd' command)
+echo "Current working directory:"
+pwd
